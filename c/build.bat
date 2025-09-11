78ac38fda8589a1e4f672993fc6b7bf008bc4a14
asiansergio <sergioleesp@gmail.com>
2025-09-11 10:33:36 +0200

    Revert "feat: make sure cl is available before build"
    This reverts commit cbebe4a8e3cbc2bb3da490da87109b556fdf887b.

--- c/build.bat
+++ c/build.bat
@@ -12,25 +12,5 @@ if "%1"=="release" (
 
 if not exist "%outdir%" mkdir "%outdir%"
 
-REM Try loading vcvars64.bat directly
-set "vs_base=%ProgramFiles%\Microsoft Visual Studio\2022"
-set "cl_found="
-
-for %%E in (Enterprise Professional Community) do (
-    if exist "%vs_base%\%%E\VC\Auxiliary\Build\vcvars64.bat" (
-        call "%vs_base%\%%E\VC\Auxiliary\Build\vcvars64.bat"
-        where cl >nul 2>&1
-        if not errorlevel 1 (
-            set "cl_found=1"
-            goto :found_cl
-        )
-    )
-)
-
-:found_cl
-if not defined cl_found (
-    exit /b 1
-)
-
 cl !flags! /Fe"%outdir%\quick.exe" /Fo"%outdir%"\ /Fd"%outdir%"\ quick.c
 
